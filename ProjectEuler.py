f0aea91951a6bd32817943ce504098b314116a3f
stonepresto <preston.pritchard@usma.edu>
2016-04-20 08:54:49 -0400

    *Problem 9
    Meant to put 'problem' on the last one, not project. One of the few
    tickers I've made

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -200,6 +200,24 @@ def LPS(anum):
 #There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 #Find the product abc.
 
+def SPT(n):
+	trips = [1,1,1]
+	while trips[0] != n/2:
+		if (trips[0]*trips[0])+(trips[1]*trips[1]) == trips[2]*trips[2]:
+			if trips[0]+trips[1]+trips[2] == n:
+				return trips[0]*trips[1]*trips[2]
+#this is kind of ugly... it's a good ticker though, kind of like a clock. Also very slow when n >>> 1 so that is why I use n/2, which assumes the largest triplet number is < n/2
+		if trips[2] < n/2:
+			trips[2] += 1
+		elif trips[2] == n/2:
+			trips[2] = 1
+			if trips[1] < n/2:
+				trips[1] += 1
+			elif trips[1] == n/2:
+				trips [1] = 1
+				if trips[0] < n/2:
+					trips[0] += 1
+
 
 
 #######################################
