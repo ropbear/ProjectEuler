caec917ebf3245d46514fba1d0fedd19a24209f1
stonepresto <preston.pritchard@usma.edu>
2016-05-02 10:37:07 -0400

    Problem 12 and 13 setup

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -327,16 +327,25 @@ def gridProd():
 
 
 ###############PROBLEM 12###############
+#What is the value of the first triangle number to have over five hundred divisors?
 
-
-
-
+def triangles(div):
+	summ = 0	
+	num = 1
+	while True:
+		summ += num
+		if summ%2 == 0:
+			dlist = list_divisors(summ)
+			if len(dlist)*2 >= div:
+				return summ
+		num += 1
+		
 
 #######################################
 
 
 ###############PROBLEM 13###############
-
+#Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 
 
 
