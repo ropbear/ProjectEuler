e1a441a6c038d91f13ee125ba92954ef10c27ff5
stonepresto <preston.pritchard@usma.edu>
2016-05-05 09:18:03 -0400

    Problem 13
    Starting to get easier, I think I might move on to some harder problems
    if they haven't changed much by problem 15

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -347,7 +347,29 @@ def triangles(div):
 ###############PROBLEM 13###############
 #Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 
+def bigs():
+	fil = open('problem13.txt','r')
+	num = 0
+	for line in fil:
+		num += int(line)
+	num = str(num)
+
+	numlist = []
+	for n in range(100):
+		numlist.append(int(num[n*50:(n+1)*50]))
+
+	return(str(sum(numlist))[:10])
+
+
+#######################################
+
+
+###############PROBLEM 14###############
+
+
 
 
 
 #######################################
+
+
