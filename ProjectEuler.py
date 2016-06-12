f438bf34bd90eb8bb104d9704070d88b93a47e51
stonepresto <preston.pritchard@usma.edu>
2016-06-12 16:08:26 -0400

    Problem 16
    Flashed it.

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -411,17 +411,33 @@ def grid(n):
 
 
 ###############PROBLEM 16##############
+#2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#What is the sum of the digits of the number 2^1000?
+def power(base,n):
+	prod = 1
+	t = 0
+	while t < n:
+		prod *= base
+		t += 1
+	ps = str(prod)
+	sumlist = []
+	for i in range(len(ps)):
+		sumlist.append(int(ps[i]))
+	return sum(sumlist)
 
 
-
-
-
-
+#SHABAM; FLASHED IT!
 
 #######################################
 
 
+###############PROBLEM 17##############
+#If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+#If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 
+#NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+#######################################
 
 
 
