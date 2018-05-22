fa9b5b9a7039eba47f9438fd11ff6ab2f048f6a1
stonepresto <prestonpdppritchard@gmail.com>
2018-05-22 19:20:32 -0400

    adding problem 18 solve

--- 017/17.py
+++ 017/17.py
@@ -1,16 +1,16 @@
-class Tree:
+class BinaryTree:
 	def __init__(self,filename,size):
+		self.size = size
 		self.layers = self.treeify(filename)
 		self.tree = self.fromList(self.layers)
-		self.size = size
 
 	def fromList(self,layers):
 		def recurse(items,i,size,max_size):
 			if size == max_size:
 				return []
 			else:
-				return [items[i],recurse(items,i+size,size+1,max_size),recurse(items,i+size+1,size+1,max_size)]
-		return recurse(layers,0,1,6)
+				return [int(items[i]),recurse(items,i+size,size+1,max_size),recurse(items,i+size+1,size+1,max_size)]
+		return recurse(layers,0,1,self.size)
 
 	def treeify(self,filename):
 		f = open(filename,'r')
@@ -20,5 +20,13 @@ class Tree:
 			layers += layer
 		return layers
 
+
+def bruteMaxPath(tree):
+	if tree == []:
+		return 0
+	else:
+		return tree[0] + max(bruteMaxPath(tree[1]),bruteMaxPath(tree[2]))
+
 if __name__ == "__main__":
-	tree = Tree("tree.txt",6)
+	tree = BinaryTree("tree.txt",16)
+	print(bruteMaxPath(tree.tree))
