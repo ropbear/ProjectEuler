7c99273f6cbbf660c969430998744200eaf7e7f0
stonepresto <prestonpdppritchard@gmail.com>
2018-06-03 16:36:13 -0500

    finishing up problem 21, good stuff

--- 020/020.py
+++ 020/020.py
@@ -1,24 +1,31 @@
-def amicable(num):
-	amicable_sum = 0
+def sum_divisors(num):
+	divisor_sum = 0
 	for i in range(1,num):
 		if num % i == 0:
-			amicable_sum += i
-	return amicable_sum
+			divisor_sum += i
+	return divisor_sum
 
 def sum_amicable_pairs(limit):
-	ams = {}
+	#pairs = [[0]*limit]*limit #create a (limit,limit) matrix for memoization
+	#This actually just makes 'limit' rows of the same object... so any change happens to them all
+	#here is the proper way...
+	pairs = [[0 for x in range(limit)] for y in range(limit)]
 	for num in range(1,limit):
-		amsum = amicable(num)
+		dsum = sum_divisors(num)
 		try:
-			ams[amsum] = ams[amsum] + [num]
+			pairs[num][dsum] = 1
 		except:
-			ams[amsum] = [num]
+			pass #ignore sums of the proper divisors that are over the limit
 
-	total = 0
-	for value in ams.values():
-		for num in value:
-			if num in ams.keys():
-				total += num + sum(ams[num])
+	total = 0 
+	#[print(x) for x in pairs] < Easy way to print a matrix
+	for x in range(limit):
+		for y in range(limit):
+			if x != y and pairs[x][y] == 1 and pairs[y][x] == 1:
+				print(x,y)
+				pairs[y][x] = 0
+				total += (x + y)
+			
 	return total
 
 if __name__ == "__main__":
